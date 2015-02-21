ce7d662594e721df9984fc3c12123e97e5db09de
Christopher Danan <chrisddan@gmail.com>
2015-02-21 13:29:23 -0800

    Remove bug where empty comments are submitted.

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ var main = function()
 	"use strict";
 
 	//window.alert("hello Vane");
-	
+
 	//Create an event listener for the button.
-	$(".comment-input button").on("click", function(event){
+	$(".comment-input button").on("click", function(event)
+	{
 		//console.log("hello Vane!");
 		var $new_comment = $("<p>"),	//Create a new paragraph element.
 			comment_text = $(".comment-input input").val(); //Stores the value of the input text box.
 
-		$new_comment.text(comment_text);	//Set the text of the new paragraph to the value of the input box.
-		$(".comments").append($new_comment);	//Put the new comment in the comments section.
+		if($(".comment-input input").val() !== "")
+		{
+			$new_comment.text(comment_text);	//Set the text of the new paragraph to the value of the input box.
+			$(".comments").append($new_comment);	//Put the new comment in the comments section.
+		}
 	});
 };
 
