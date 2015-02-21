7ac1038071b25c181b843ed363d3cce7a9c76907
Christopher Danan <chrisddan@gmail.com>
2015-02-21 12:29:58 -0800

    Re-link app.js in index.html. Add event listener when button is pressed. -It prints "hello Vane!" to console.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var main = function(){
+var main = function()
+{
 	"use strict";
 
-	window.alert("hello Vane");
+	//window.alert("hello Vane");
+	$(".comment-input button").on("click", function(event){
+		console.log("hello Vane!");
+	});
 };
 
 $(document).ready(main);
