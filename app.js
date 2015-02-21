1d9c5a4d363a802a71b1dd00a2b1de07aa15c311
Christopher Danan <chrisddan@gmail.com>
2015-02-21 13:18:18 -0800

    Modify app.js so that when user presses submit button, the text that user input into input text box shows up as the newly submitted comment's text.

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var main = function()
 	"use strict";
 
 	//window.alert("hello Vane");
+	
+	//Create an event listener for the button.
 	$(".comment-input button").on("click", function(event){
 		//console.log("hello Vane!");
-		$(".comments").append("<p>This is a new comment!</p>");
+		var $new_comment = $("<p>"),	//Create a new paragraph element.
+			comment_text = $(".comment-input input").val(); //Stores the value of the input text box.
+
+		$new_comment.text(comment_text);	//Set the text of the new paragraph to the value of the input box.
+		$(".comments").append($new_comment);	//Put the new comment in the comments section.
 	});
 };
 
